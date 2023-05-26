cd72d65d6a007a6c9c1bf3e4603ae24ab58e908f
Matías Vega <elmativega3@gmail.com>
2023-05-25 21:33:04 -0300

    dinamismo al textWithImage y un poco de mejoras al css

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const myContent =[
       'Descripción parrafo 3. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.'
     ],
     finishingtext:'FinishingText parrafo 4. Texto a completar dependiendo el material que se a mostrado',
-    img:'https://www.bing.com/images/search?view=detailV2&ccid=kxheDlMT&id=31CB7CCAE1DF10EC9D6E4BE174E0955798F1F718&thid=OIP.kxheDlMTKSTxr1I0Kkg3IgAAAA&mediaurl=https%3a%2f%2fassets.digitalocean.com%2farticles%2fUnicorn_Ruby%2fimg1.png&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.93185e0e53132924f1af52342a483722%3frik%3dGPfxmFeV4HThSw%26pid%3dImgRaw%26r%3d0&exph=205&expw=200&q=imagen+unicornio+error+de+conexion+github&simid=608000604615240012&FORM=IRPRST&ck=4AD5F0CE53A1285CDF58C1D579EC3362&selectedIndex=8',
+    img:'https://th.bing.com/th/id/OIP.73hX3ZqIR5yzxBrTL3wC9QHaJ4?w=123&h=180&c=7&r=0&o=5&pid=1.7',
   },
   {
     title:'PROFESION 2 COMO TITULO',
@@ -20,7 +20,7 @@ const myContent =[
       'Descripción parrafo 3. Ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.'
     ],
     finishingtext:'FinishingText parrafo 4. Texto a completar dependiendo el material que se a mostrado',
-    img:'https://www.bing.com/images/search?view=detailV2&ccid=mi6N37te&id=8B14AF2D8E8E778C3BA5C3B0B7E1653285613735&thid=OIP.mi6N37te_8TrmvFaP561-gAAAA&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.9a2e8ddfbb5effc4eb9af15a3f9eb5fa%3frik%3dNTdhhTJl4bewww%26riu%3dhttp%253a%252f%252fcutchet.com.ar%252fimg%252fpaisaje%252fpaisaje-actual.png%26ehk%3dKYAi%252fzAfU4wmPUNrrLCseJ2SlMmx2e6cFvoQwqY6vyc%253d%26risl%3d%26pid%3dImgRaw%26r%3d0&exph=397&expw=318&q=cacao+el+quilla+imagen&simid=608023956369527634&FORM=IRPRST&ck=182946A531609DFEAB34D8F48E238F02&selectedIndex=166',
+    img:'https://th.bing.com/th/id/OIP.StKy4NQsKBnLe7Zh6fo3YAHaLD?w=115&h=180&c=7&r=0&o=5&pid=1.7',
   },
 ]
 
@@ -29,8 +29,9 @@ function App() {
     <div className="root">
       <NavBar />
       <CenteresText />
-      <TextWithImage textToTheRight={false} />
-      <TextWithImage textToTheRight={true} />
+      {
+        myContent.map((item, index)=>(<TextWithImage textToTheRight={index%2===0} content={item} />))
+      }
     </div>
   );
 }
--- src/components/textWithImage.js
+++ src/components/textWithImage.js
@@ -1,92 +1,126 @@
 import React from "react";
 import { Grid, Typography } from "@mui/material";
-import logo from "../logo.svg";
 import "../styles/textWithImage.css";
-
-const TextToTheRight = ()=>{
+const Description = (props) => {
+  return (
+    <Grid item xs spacing={3}>
+      <Typography variant="body1">{props.description}</Typography>
+    </Grid>
+  );
+};
+const TextToTheRight = (props) => {
+  
+ 
   return (
     <Grid
+    className="gridTextToTheRigth"
       container
       direction="row"
-      justify="space-around"
       alignItems="center"
       spacing={3}
     >
       <Grid item xs={4}>
-        <img src={logo} alt="Esto es una imagen" width="700" height="700"></img>
+        <img
+          src={props.content.img}
+          alt="Esto es una imagen"
+          width="700"
+          height="700"
+        ></img>
       </Grid>
       <Grid item xs={4}>
-        <Grid
-          container
-          direction="column"
-          alignItems="center"
-          spacing={3}
-        >
+        <Grid container direction="column" alignItems="center" spacing={3}>
           <Grid item xs>
-              <Typography variant="h6">PROFESION COMO TITULO</Typography>
+            <Typography variant="h6">{props.content.title}</Typography>
           </Grid>
           <Grid item xs spacing={3}>
-              <Typography variant="body1"><b>Subtítulo parrafo 1.<br/>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</b></Typography>
+            <Typography variant="body1">
+              <b>{props.content.subtitle}</b>
+            </Typography>
           </Grid>
+          
+          {props.content.descriptions.map((description)=>(
+            <Description description={description}/>
+          ))}
+
           <Grid item xs spacing={3}>
-              <Typography variant="body1">Descripción parrafo 2.<br/>Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</Typography>
-          </Grid>
-          <Grid item xs spacing={3}>
-              <Typography variant="body1"><i>Descripción parrafo 3.<br/> No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.</i></Typography>
-          </Grid>
-          <Grid item xs spacing={3}>
-              <Typography variant="body1"><i>FinishingText parrafo 4.<br/> Texto a completar dependiendo el material que se a mostrado</i></Typography>
+            <Typography variant="body1">
+              <i>
+                FinishingText parrafo 4.
+                <br /> Texto a completar dependiendo el material que se a
+                mostrado
+              </i>
+            </Typography>
           </Grid>
         </Grid>
       </Grid>
     </Grid>
   );
-}
+};
 
-const TextToTheLeft = ()=>{
+const TextToTheLeft = (props) => {
   return (
     <Grid
+      className="gridTextToTheLeft"
       container
-      direction="row"
-      justify="space-around"
+      direction="row"      
       alignItems="center"
       spacing={3}
     >
       <Grid item xs={4}>
-        <Grid
-          container
-          direction="column"
-          alignItems="center"
-          spacing={3}
-        >
+        <Grid container direction="column" alignItems="center" spacing={3}>
           <Grid item xs>
-              <Typography variant="h6">PROFESION COMO TITULO</Typography>
+            <Typography variant="h6">{props.content.title}</Typography>
           </Grid>
           <Grid item xs spacing={3}>
-              <Typography variant="body1"><b>Subtítulo parrafo 1.<br/>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</b></Typography>
+            <Typography variant="body1">
+            {props.content.subtitle}
+            </Typography>
+          </Grid>
+          {props.content.descriptions.map((description)=>(
+            <Description description={description}/>
+          ))}
+          {/* <Grid item xs spacing={3}>
+            <Typography variant="body1">
+              Descripción parrafo 2.
+              <br />
+              Lorem Ipsum ha sido el texto de relleno estándar de las industrias
+              desde el año 1500, cuando un impresor (N. del T. persona que se
+              dedica a la imprenta) desconocido usó una galería de textos y los
+              mezcló de tal manera que logró hacer un libro de textos especimen.
+            </Typography>
           </Grid>
           <Grid item xs spacing={3}>
-              <Typography variant="body1">Descripción parrafo 2.<br/>Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</Typography>
-          </Grid>
+            <Typography variant="body1">
+              <i>
+                Descripción parrafo 3.
+                <br /> No sólo sobrevivió 500 años, sino que tambien ingresó
+                como texto de relleno en documentos electrónicos, quedando
+                esencialmente igual al original.
+              </i>
+            </Typography>
+          </Grid> */}
           <Grid item xs spacing={3}>
-              <Typography variant="body1"><i>Descripción parrafo 3.<br/> No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.</i></Typography>
-          </Grid>
-          <Grid item xs spacing={3}>
-              <Typography variant="body1"><i>FinishingText parrafo 4.<br/> Texto a completar dependiendo el material que se a mostrado</i></Typography>
+            <Typography variant="body1">
+              <i>
+                FinishingText parrafo 4.
+                <br /> Texto a completar dependiendo el material que se a
+                mostrado
+              </i>
+            </Typography>
           </Grid>
         </Grid>
       </Grid>
       <Grid item xs={4}>
-        <img src={logo} alt="Esto es una imagen" ></img>
+        <img src={props.content.img} alt="Esto es una imagen"></img>
       </Grid>
     </Grid>
   );
-}
-
-
+};
 
 export function TextWithImage(props) {
-  
-   return props.textToTheRight? <TextToTheRight/>:<TextToTheLeft/>
-  
+  return props.textToTheRight ? (
+    <TextToTheRight content={props.content} />
+  ) : (
+    <TextToTheLeft content={props.content} />
+  );
 }
